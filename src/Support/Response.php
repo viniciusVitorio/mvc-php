bae40c494a25a62e4c24031d742fcd4c483edd2f
ViniciusVItorio <vini.vtj@gmail.com>
2025-09-09 22:05:38 -0300

    feat: adiciona `Person/Response.php`

---
+++ src/Support/Response.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src\Support;
+
+class Response
+{
+    public static function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    public static function redirect(string $url, int $status = 302): void
+    {
+        http_response_code($status);
+        header('Location: ' . $url);
+        exit;
+    }
+}
